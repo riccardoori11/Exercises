3011edbc63d1f572b8819daa3d27da2c28936008
Riccardo <riccardoori281@gmail.com>
2026-06-07 20:17:49 -0400

    Changed name to pingpong

--- Exercise3.cpp
+++ Exercise3.cpp
@@ -6,18 +6,18 @@
 std::binary_semaphore foo_semaphore{1};
 std::binary_semaphore bar_semaphore{0};
 
-auto foo(int n){
+auto ping(int n){
 		
 		while (n-->0){
 				foo_semaphore.acquire();
-				std::osyncstream(std::cout) << "foo";
+				std::osyncstream(std::cout) << "ping";
 				bar_semaphore.release();
 		}
 }
-auto bar(int n){
+auto pong(int n){
 		while (n-->0){
 				bar_semaphore.acquire();
-				std::osyncstream(std::cout) << "bar" << "\n";
+				std::osyncstream(std::cout) << "pong" << "\n";
 				foo_semaphore.release();
 		}
 }
@@ -26,8 +26,8 @@ int main(){
 		int n{1000};
 		
 		
-		std::thread t1(foo,n);
-		std::thread t2(bar,n);
+		std::thread t1(ping,n);
+		std::thread t2(pong,n);
 
 		t1.join();
 		t2.join();
